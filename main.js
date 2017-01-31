161a49be01d4c9af268bbc94c94857ed1f2d55c8
Viren Bhagat <virenb@gmail.com>
2017-01-31 14:25:54 -0500

    Updated calls to $.ajax

--- main.js
+++ main.js
@@ -1,74 +1,59 @@
-$(document).ready(function(){
-    var following = [];
+$(document).ready(function () {
 
-    $.ajax({
-        type: "GET",
-        url: "https://api.twitch.tv/kraken/streams/freecodecamp",
-        headers: {
-            'Client-ID': 'br77xr7cxh06qe2w4tar7hf9gp812k'
-        },
-        success: function(data1) {
-            if(data1.stream === null) {
-                $("#fcc-status").html("Free Code Camp is currently OFFLINE");
-            }
-            else {
-                $("#fcc-status").html("Free Code Camp is currently ONLINE");
-            }
-        }
-    });
+	$.ajax({
+		type: "GET",
+		url: "https://api.twitch.tv/kraken/streams/freecodecamp",
+		headers: {
+			'Client-ID': 'br77xr7cxh06qe2w4tar7hf9gp812k'
+		},
+		success: function (data1) {
+			if (data1.stream === null) {
+				$("#fcc-status").html("Free Code Camp is currently OFFLINE");
+			} else {
+				$("#fcc-status").html("Free Code Camp is currently ONLINE");
+			}
+		}
+	});
 
-    var followerURL = "https://api.twitch.tv/kraken/users/freecodecamp/follows/channels/";
-    $.getJSON(followerURL, function(data2) {
-        for (var i = 0; i < data.follows.length; i++) {
-            var displayName = data2.follows[i].channel.display_name;
-            following.push(displayName);
-        }
-        following.push('comster404');
-        following.push('brunofin');
-        following.push('ESL_SC2');
+	$.ajax({
+		type: "GET",
+		url: "https://api.twitch.tv/kraken/users/freecodecamp/follows/channels/",
+		headers: {
+			'Client-ID': 'br77xr7cxh06qe2w4tar7hf9gp812k'
+		},
+		success: function (data2) {
+			for (var i = 0; i < data2.follows.length; i++) {
+				var displayName = data2.follows[i].channel.display_name;
+				var logo = data2.follows[i].channel.logo;
+				var status = data2.follows[i].channel.status;
 
-        for (var i = 0; i < following.length ;i++) {
-            var url2 = 'https://api.twitch.tv/kraken/streams/' + following[i] + '/?callback=?';
+				if (logo == null) {
+					logo = 'https://www.coachimo.de/static/images/icon_not_found.png';
+				}
+				$("#followerInfo").prepend("<div class ='row'>" + "<div class='col-md-4'>" +
+					"<img src='" + logo + "'>" +
+					"</div>" + "<div class='col-md-4'>" + displayName + "</div>" + "<div class='col-md-4'>" + status + "</div></div>");
+			}
+		}
+	});
 
-            $.getJSON(url2, function(data3) {
-                var logo;
-                var status;
-                var name;
-                if (data3.error) {
-                    logo = "Insert Logo";
-                    name = data3.message;
-                    status = data3.error;
-
-                    $("#followerInfo").prepend("<div class = 'row'>" + "<div class='col-md-4'>" + "<img src='" + logo +"'>" +
-                        "</div>" + "<div class='cold-md-4'>" + name + "</div>" + "<div class='col-md-4'>" + status + "</div></div>");
-                }
-                if (data3.stream == null) {
-                    $.getJSON(data3._links.channel, function(data5) {
-                        status = 'OFFLINE';
-                        logo = data5.logo;
-                        name = data3.display_name;
-                        if (logo === null) {
-                            logo = 'http://web.vmc3.com/projects/bufs/branch/marines/logo/NoLogo.jpg';
-                        }
-                    $("#followerInfo").prepend("<div class = 'row'>" + "<div class='col-md-4'>" + "<img src='" + logo +"'>" +
-                        "</div>" + "<div class='cold-md-4'>" + name + "</div>" + "<div class='col-md-4'>" + status + "</div></div>");
-                    });
-                }
-            });
-        }
-        for (var i = 0; i < following.length; i++) {
-            var onlineURL = 'https://api.twitch.tv/kraken/streams/' + following[i];
-            $.getJSON(onlineURL, function(data4) {
-                    var logo = data4.stream.channel.logo;
-                if (logo === null) {
-                    logo = 'http://web.vmc3.com/projects/bufs/branch/marines/logo/NoLogo.jpg';
-                }
-                    var status = data4.stream.channel.status ;
-                    var name = data4.stream.channel.display_name;
-
-                $("#followerInfo").prepend("<div class = 'row'>" + "<div class='col-md-4'>" + "<img src='" + logo +"'>" +
-                    "</div>" + "<div class='cold-md-4'>" + name + "</div>" + "<div class='col-md-4'>" + status + "</div></div>");
-            });
-        }
-    });
+	var deletedFollowers = ['brunofin', 'comster404'];
+	for (var i = 0; i < deletedFollowers.length; i++) {
+		$.ajax({
+			type: "GET",
+			url: "https://api.twitch.tv/kraken/streams" + deletedFollowers[i],
+			headers: {
+				'Client-ID': 'br77xr7cxh06qe2w4tar7hf9gp812k'
+			},
+			error: function (data3) {
+				var logo = 'https://www.coachimo.de/static/images/icon_not_found.png';
+				var displayName = data3.statusText;
+				console.log(data3.statusText);
+				var status = data3.status;
+				$("#followerInfo").prepend("<div class ='row'>" + "<div class='col-md-4'>" +
+					"<img src='" + logo + "'>" +
+					"</div>" + "<div class='col-md-4'>" + displayName + "</div>" + "<div class='col-md-4'>" + status + "</div></div>");
+			}
+		});
+	}
 });
